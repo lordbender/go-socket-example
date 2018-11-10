08ad948579306004c4084cfc28fa6f4e97432f45
Sean Willison <sean.willison@gmail.com>
2018-11-10 15:40:30 -0500

    working on rocks distribution

--- main.go
+++ main.go
@@ -10,40 +10,54 @@ import (
 )
 
 func report(algorithm, elapsed string, size int) {
-	log.Printf("%s Sort took %s\n\tSize: %d\n\n", algorithm, elapsed, *size)
+	log.Printf("%s Sort took %s\n\tSize: %d\n\n", algorithm, elapsed, size)
 }
 
 // Citation: https://systembash.com/a-simple-go-tcp-server-and-tcp-client/
 func main() {
-	size := flag.Int("--size", 1000, "Size of the test set.")
+	size := flag.Int("size", 1000, "Size of the test set.")
+	all := flag.Bool("all", false, "If true, all reports will run")
+	mergeSortReports := flag.Bool("nlogn", false, "If true, all merge sort actions are triggered, report run.")
+	squaredReports := flag.Bool("nsquared", false, "If true, all merge sort actions are triggered, report run.")
+
 	flag.Parse()
 
-	a := core.GetArray(*size)
-	start := time.Now()
-	services.MergeSort(a)
-	elapsed := time.Since(start)
-	report("Sequential Merge Sort", elapsed.String(), *size)
+	if *all || *mergeSortReports {
+		a := core.GetArray(*size)
+		start := time.Now()
+		services.MergeSort(a)
+		elapsed := time.Since(start)
+		report("Sequential Merge Sort", elapsed.String(), *size)
+	}
 
-	b := core.GetArray(*size)
-	parallelMergeSortStart := time.Now()
-	services.MergeSortParallel(b, 50)
-	parallelMergeSortElapsed := time.Since(parallelMergeSortStart)
-	report("Parallel Merge Sort", parallelMergeSortElapsed.String(), *size)
+	if *all || *mergeSortReports {
+		b := core.GetArray(*size)
+		parallelMergeSortStart := time.Now()
+		services.MergeSortParallel(b, 50)
+		parallelMergeSortElapsed := time.Since(parallelMergeSortStart)
+		report("Parallel Merge Sort", parallelMergeSortElapsed.String(), *size)
+	}
 
-	c := core.GetMatrix(*size, *size, true)
-	squareMatrixStart := time.Now()
-	services.SquareMatrix(c)
-	squareMatrixEnd := time.Since(squareMatrixStart)
-	report("Sequential Square Matrix", squareMatrixEnd.String(), *size)
+	if *all || *squaredReports {
+		c := core.GetMatrix(*size, *size, true)
+		squareMatrixStart := time.Now()
+		services.SquareMatrix(c)
+		squareMatrixEnd := time.Since(squareMatrixStart)
+		report("Sequential Square Matrix", squareMatrixEnd.String(), *size)
+	}
 
-	d := core.GetMatrix(*size, *size, true)
-	squareMatrixParStart := time.Now()
-	services.SquareMatrixParallel(d)
-	squareMatrixParEnd := time.Since(squareMatrixParStart)
-	report("Parallel Square Matrix", squareMatrixParEnd.String(), *size)
+	if *all || *squaredReports {
+		d := core.GetMatrix(*size, *size, true)
+		squareMatrixParStart := time.Now()
+		services.SquareMatrixParallel(d)
+		squareMatrixParEnd := time.Since(squareMatrixParStart)
+		report("Parallel Square Matrix", squareMatrixParEnd.String(), *size)
+	}
 
-	rocksMergeSortStart := time.Now()
-	rocks.Greet()
-	rocksMergeSortElapsed := time.Since(rocksMergeSortStart)
-	report("Distributed Merge Sort", rocksMergeSortElapsed.String(), *size)
+	if *all || *mergeSortReports {
+		rocksMergeSortStart := time.Now()
+		rocks.Greet()
+		rocksMergeSortElapsed := time.Since(rocksMergeSortStart)
+		report("Distributed Merge Sort", rocksMergeSortElapsed.String(), *size)
+	}
 }

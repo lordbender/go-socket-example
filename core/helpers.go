29ebf796a7880a869c4958de0383850df0902e12
Sean Willison <sean.willison@gmail.com>
2018-11-10 12:20:08 -0500

    initial commit

---
+++ core/helpers.go
@@ -0,0 +1,7 @@
+package core
+
+// Citation: https://systembash.com/a-simple-go-tcp-server-and-tcp-client/
+func getArray() []int {
+
+	return make([]int, 100)
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "oop/core"
+
+// Citation: https://systembash.com/a-simple-go-tcp-server-and-tcp-client/
+func main() {
+
+	a := core.getArray()
+	b := core.getArray()
+
+	fmt.Println("Launching server...")
+}
